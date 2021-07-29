f12705dbebd033dd80ea46a20e779828b7ab27c3
Cida Castello <cidacastello.cursos@gmail.com>
2021-07-29 14:31:58 -0300

    Implementação dos DTOs

--- src/main/java/com/devsuperior/movieflix/repositories/MovieRepository.java
+++ src/main/java/com/devsuperior/movieflix/repositories/MovieRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.devsuperior.movieflix.entities.Movie;
 
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+	
+	
 
 }
